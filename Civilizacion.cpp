9af6f527b719eefcdc105c4ba559261178dc571c
Erick Carrasco <erick.e.carrasco88@gmail.com>
2018-12-07 13:45:03 -0600

    Ultima actualizacion en la clase Civilizacion.cpp 13:44

--- Civilizacion.cpp
+++ Civilizacion.cpp
@@ -24,3 +24,34 @@ Civilizacion::Civilizacion(string nombre, string planeta, string lider, int edad
 	this->edad=edad;
 }
 
+string Civilizacion::getNombre(){
+	return nombre;
+}
+
+void Civilizacion::setNombre(string nombre){
+	this->nombre=nombre;
+}
+
+string Civilizacion::getPlaneta(){
+	return planeta;
+}
+
+void Civilizacion::setPlaneta(string planeta){
+	this->planeta=planeta;
+}
+
+string Civilizacion::getLider(){
+	return lider;
+}
+
+void Civilizacion::setLider(string lider){
+	this->lider=lider;
+}
+
+int Civilizacion::getEdad(){
+	return edad;
+}
+
+void Civilizacion::setEdad(int edad){
+	this->edad=edad;
+}
