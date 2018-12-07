7b7825eb97f3f5d88774adf9fdb96de3703698f1
Joseph Moscoso <josephmoscoso70@hotmail.com>
2018-12-07 16:38:10 -0600

    Binario

--- Civ_BB01.cpp
+++ Civ_BB01.cpp
@@ -5,12 +5,47 @@ Civ_BB01::Civ_BB01(string nombre, string planeta, string lider, int edad):Civili
 }
 
 string Civ_BB01::Codificar(string x){
-  
-  return "";
+  x+='-';
+  string final;
+  string temp;
+  for (int i = 0; i < s.size(); i++) {
+    if (x.at(i)!='-') {
+      temp+=x.at(i);
+    }else if(x.at(i)=='-'){
+      int z=atoi(temp.c_str());
+      int decNum = 0;
+      int v=1;
+      int rem=0;
+      while (z!=0) {
+          rem=z%10;
+          decNum=decNum+rem*v;
+          v*=2;
+          z/=10;
+      }
+      char cosas=static_cast<char>(decNum);
+      final+=cosas;
+      temp.clear();
+    }
+  }
+  return final;
 }
 
 string Civ_BB01::Decodificar(string x){
-  return "";
+  string final;
+  int b=0;
+  for (int i = 0; i < x.size(); i++) {
+    char cac=x.at(i);
+    int num=cac;
+    int a[7];
+    int j=0;
+    while (num>0) {
+      b=num%2;
+      final+=std::to_string(bin);
+      num/=2;
+    }
+    final+='-';
+  }
+  return final;
 }
 
 Civ_BB01::~Civ_BB01(){
